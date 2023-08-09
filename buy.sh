f0fcb574e164c06c5aa5effd2cdfd99e4f5fe640
ed <>
2023-08-09 13:37:50 +0200

    cancel appropritate side
    Signed-off-by: ed <>

--- buy.sh
+++ buy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-yarn ts-node examples/cli.ts cancel $1 & yarn ts-node examples/cli.ts split buy $1 10000$ 50 last-$2% last+0.1%
+yarn ts-node examples/cli.ts cancel buy $1 & yarn ts-node examples/cli.ts split buy $1 10000$ 50 last-$2% last+0.1%
